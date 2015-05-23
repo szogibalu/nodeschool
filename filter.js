ef485e2c4d6c1aafbfeeadb5c76f58a61d693db0
szogi <szogibalu@gmail.com>
2015-05-23 11:04:54 +0200

    Added new finished tasks

---
+++ filter.js
@@ -0,0 +1,11 @@
+function getShortMessages(messages) {
+    return messages
+        .map(function(message) {
+            return message.message
+         })
+        .filter(function(message) {
+            return message.length < 50;
+        });
+}
+
+module.exports = getShortMessages
