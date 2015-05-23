77933255de13b9e34c847d35f9a2130dfb29cb55
szogi <szogibalu@gmail.com>
2015-05-23 11:23:05 +0200

    Added new exercises

---
+++ reduce.js
@@ -0,0 +1,13 @@
+    function countWords(inputWords) {
+      return inputWords.reduce(function(map, word){
+            console.log(map);
+            if(map[word]) {
+                map[word] = ++map[word];
+            } else {
+                map[word] = 1;
+            }
+         return map;
+      }, {});
+    }
+
+    module.exports = countWords
